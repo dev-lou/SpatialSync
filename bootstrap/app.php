321d573af7281908b219fa8cf5e5e0ed1a1f10bd
Lou <loubaroro@isufst.edu.ph>
2026-05-05 11:09:52 +0800

    SEO: Implement HTTPS enforcement and register AppServiceProvider for production stability

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withProviders([
+        \App\Providers\AppServiceProvider::class,
         ViewServiceProvider::class,
     ])
     ->withMiddleware(function (Middleware $middleware) {
